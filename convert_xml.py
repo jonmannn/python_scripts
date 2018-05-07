277fcac5a9d3831344a445279bb95cd3a9958ddf
jonmannn <jonmannn@gmail.com>
2018-05-07 15:24:30 -0400

    added xml converter
    converts newlines to windwos style

---
+++ convert_xml.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+convert_xml.py Takes convert to Windows style newlines from provided mac/unix file
+"""
+
+import sys
+
+if len(sys.argv) < 2:
+    print('Error: No file specified. Please supply the full path to the file you are trying to convert')
+    exit()
+
+filename = sys.argv[1]
+
+fileContents = open(filename, "r").read()
+f = open(filename, "w", newline="\r\n")
+f.write(fileContents)
+f.close()
